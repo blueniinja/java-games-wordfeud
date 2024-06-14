ee2397c5b0d975e9ab47bd3668ed559d5bf0834f
blueniinja <bluehats2@outlook.com>
2024-06-14 17:26:00 +0200

    Add getters for letter panel and impl accept, undo and clear buttons

--- src/main/java/com/jakobniinja/LetterPanel.java
+++ src/main/java/com/jakobniinja/LetterPanel.java
@@ -10,10 +10,10 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class LetterPanel extends JFrame {
+public class LetterPanel extends JPanel {
 
   private static final long serialVersionUID = 1L;
 
@@ -25,6 +25,8 @@ public class LetterPanel extends JFrame {
 
   private int points = -1;
 
+  private int column = -1;
+
   private BufferedImage image = null;
 
   private int size = 40;
@@ -34,6 +36,7 @@ public class LetterPanel extends JFrame {
   private Font smallFont = new Font(Font.DIALOG, Font.BOLD, 12);
 
   private FontMetrics bigFM;
+
   private FontMetrics smallFM;
 
 
@@ -60,6 +63,43 @@ public class LetterPanel extends JFrame {
     smallFM = getFontMetrics(smallFont);
   }
 
+  public String getLetter() {
+    return letter;
+  }
+
+  public int getPoints() {
+    return points;
+  }
+
+  public int getColumn() {
+    return column;
+  }
+
+  public int getPanelSize() {
+    return size;
+  }
+
+  public void setColumn(int column) {
+    this.column = column;
+  }
+
+  public void setEmpty() {
+    letter = "";
+    points = -1;
+    repaint();
+  }
+
+  public boolean isEmpty() {
+    return (points == -1);
+  }
+
+  public void copy(LetterPanel letterPanel2) {
+    letter = letterPanel2.getLetter();
+    points = letterPanel2.getPoints();
+    column = letterPanel2.getColumn();
+    repaint();
+  }
+
   public void paintComponent(Graphics g) {
     if (letter.length() == 0) {
       g.setColor(BROWN);
@@ -76,14 +116,16 @@ public class LetterPanel extends JFrame {
       g.drawRect(0, 0, size - 1, size - 1);
 
       g.setFont(bigFont);
-      int x = 5;
+      int letterWidth = bigFM.stringWidth("" + letter);
+      int x = (size - letterWidth) / 2;
       int y = size * 3 / 4;
       g.drawString(letter, x, y);
 
       g.setFont(smallFont);
-      x = size - 12;
+      letterWidth = smallFM.stringWidth("" + points);
+      x = size - letterWidth - 2;
       y = size * 17 / 20;
-      g.drawString(""+points, x,y);
+      g.drawString("" + points, x, y);
     }
   }
 
--- src/main/java/com/jakobniinja/WordBuilder.java
+++ src/main/java/com/jakobniinja/WordBuilder.java
@@ -2,8 +2,18 @@ package com.jakobniinja;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,11 +30,48 @@ public class WordBuilder extends JFrame {
 
   private static final int MAX = 15;
 
+  private static final String FILENAME = "highScore.txt";
+
+  private static final Color TAN = new Color(222, 191, 168);
+
+  private static final Font SMALLFONT = new Font(Font.DIALOG, Font.PLAIN, 12);
+
+  private static final Font BIGFONT = new Font(Font.DIALOG, Font.BOLD, 30);
+
   private LetterPanel[][] board = new LetterPanel[ROWS][COLS];
+
+  private LetterPanel[] played = new LetterPanel[MAX];
+
+  private int points = 0;
+
+  private int score = 0;
+
+  private String word = "";
+
+  Dictionary dictionary = new Dictionary();
 
   private JPanel mainPanel = new JPanel();
 
   private JPanel boardPanel = new JPanel();
+
+  private JPanel scorePanel = new JPanel();
+
+  private JPanel playPanel = new JPanel();
+
+  private JLabel pointsTitleLabel = new JLabel("Points: ");
+
+  private JLabel scoreTitleLabel = new JLabel("Score: ");
+
+  private JLabel pointsLabel = new JLabel("0");
+
+  private JLabel scoreLabel = new JLabel("0");
+
+  private JButton acceptButton = new JButton("Accept");
+
+  private JButton undoButton = new JButton("Undo");
+
+  private JButton clearButton = new JButton("Clear");
+
 
   public WordBuilder() {
     initGUI();
@@ -41,15 +88,48 @@ public class WordBuilder extends JFrame {
     add(titleLabel, BorderLayout.PAGE_START);
 
     // main panel
+    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+    mainPanel.setBackground(TAN);
     add(mainPanel, BorderLayout.CENTER);
 
     // score panel
+    scorePanel.setLayout(new BoxLayout(scorePanel, BoxLayout.X_AXIS));
+    scorePanel.setBackground(TAN);
+    mainPanel.add(scorePanel);
+
+    pointsTitleLabel.setFont(SMALLFONT);
+    scorePanel.add(pointsTitleLabel);
+
+    pointsLabel.setFont(BIGFONT);
+    scorePanel.add(pointsLabel);
+
+    Dimension boxSize = new Dimension(20, 0);
+    Component box = Box.createRigidArea(boxSize);
+    scorePanel.add(box);
+
+    scoreTitleLabel.setFont(SMALLFONT);
+    scorePanel.add(scoreTitleLabel);
+
+    scoreLabel.setFont(BIGFONT);
+    scorePanel.add(scoreLabel);
 
     // play panel
+    playPanel.setLayout(new GridLayout(1, MAX));
+    playPanel.setBackground(TAN);
+    mainPanel.add(playPanel);
+
+    for (int i = 0; i < MAX; i++) {
+      LetterPanel letterPanel = new LetterPanel();
+      played[i] = letterPanel;
+      playPanel.add(letterPanel);
+    }
 
     // board panel
     boardPanel.setBackground(Color.BLACK);
     boardPanel.setLayout(new GridLayout(ROWS, COLS));
+    int panelSize = played[0].getPanelSize();
+    Dimension maxSize = new Dimension(COLS * panelSize, ROWS * panelSize);
+    boardPanel.setMaximumSize(maxSize);
     mainPanel.add(boardPanel);
 
     BagOfLetters letters = new BagOfLetters();
@@ -58,14 +138,109 @@ public class WordBuilder extends JFrame {
         LetterPanel letterPanel = letters.pickALetter();
         board[row][col] = letterPanel;
         boardPanel.add(letterPanel);
-
       }
     }
-    
+
     // button panel
+    JPanel buttonPanel = new JPanel();
+    buttonPanel.setBackground(Color.BLACK);
+    mainPanel.add(buttonPanel, BorderLayout.PAGE_END);
+
+    acceptButton.setEnabled(false);
+    acceptButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        accept();
+      }
+    });
+
+    buttonPanel.add(acceptButton);
+
+    undoButton.setEnabled(false);
+    buttonPanel.add(undoButton);
+
+    clearButton.setEnabled(false);
+    buttonPanel.add(clearButton);
+
+    JButton endButton = new JButton("End Game");
+    buttonPanel.add(endButton);
+
+    for (int row = 0; row < ROWS; row++) {
+      for (int col = 0; col < COLS; col++) {
+        LetterPanel letterPanel = letters.pickALetter();
+        letterPanel.setColumn(col);
+        board[row][col] = letterPanel;
+        boardPanel.add(letterPanel);
+      }
+    }
+
+    for (int col = 0; col < COLS; col++) {
+      board[0][col].addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseReleased(MouseEvent e) {
+          LetterPanel letterPanel = (LetterPanel) e.getSource();
+          click(letterPanel);
+        }
+      });
+
+    }
 
     // listeners
 
+  }
+
+  private void accept() {
+    int newPoints = points * word.length();
+    score += newPoints;
+    scoreLabel.setText(""+score);
+
+    for (int i = 0; i < word.length(); i++) {
+      played[i].setEmpty();
+    }
+    points =0;
+    word = "";
+    updateButtonsAndPoints();
+  }
+
+  private void click(LetterPanel letterPanel) {
+    int wordLength = word.length();
+    if (!letterPanel.isEmpty() && wordLength < MAX) {
+      played[wordLength].copy(letterPanel);
+      word += letterPanel.getLetter();
+      points += letterPanel.getPoints();
+
+      int col = letterPanel.getColumn();
+      for (int row = 0; row < ROWS - 1; row++) {
+        board[row][col].copy(board[row + 1][col]);
+      }
+      board[ROWS - 1][col].setEmpty();
+      updateButtonsAndPoints();
+    }
+  }
+
+  private void updateButtonsAndPoints() {
+    if (word.length() == 0) {
+      acceptButton.setEnabled(false);
+      undoButton.setEnabled(false);
+      clearButton.setEnabled(false);
+      pointsLabel.setText("0");
+    } else if (word.length() < 3) {
+      acceptButton.setEnabled(false);
+      undoButton.setEnabled(true);
+      clearButton.setEnabled(true);
+      pointsLabel.setText("0");
+    } else {
+      if (dictionary.isAWord(word)) {
+        acceptButton.setEnabled(true);
+      } else {
+        acceptButton.setEnabled(false);
+      }
+
+      undoButton.setEnabled(true);
+      clearButton.setEnabled(true);
+      int newPoints = points * word.length();
+      pointsLabel.setText(""+ newPoints);
+    }
   }
 
   public static void main(String[] args) {
